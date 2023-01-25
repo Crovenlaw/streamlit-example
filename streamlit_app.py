6e8f64b3069c24022b13c578a5f1aa0334361a5f
Crovenlaw <97577997+Crovenlaw@users.noreply.github.com>
2023-01-25 13:45:24 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,5 +37,4 @@ with st.echo(code_location='below'):
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
 
-    
-   print("Hello World!")
+print("Hello World!")
